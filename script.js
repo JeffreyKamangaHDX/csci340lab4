00b92e9236474fa5351b59c7c92e1d9a396019b8
JeffreyKamangaHDX <kamangajr@hendrix.edu>
2026-02-09 11:52:51 -0600

    Was saving files from a different folder

---
+++ script.js
@@ -0,0 +1,71 @@
+$(document).ready(function(){
+    var x = 0;
+    function getRandomInt(min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    };
+
+    function rockPaperScissor(num) {
+        if (num == 1){
+            return "rock";
+        }
+        if (num == 2){
+            return "paper";
+        }
+        else{
+            return "scissors";
+        }
+    };
+    //rock choice//
+    $('#rock').click(function(){
+        x = getRandomInt(1,3);
+        $.ajax({
+        url: `https://rps101.pythonanywhere.com/api/v1/match?object_one=rock&object_two=${rockPaperScissor(x)}`,
+        success: function(results){
+            console.log(x)
+            console.log(results);
+            if (results[winner] == 'Rock'){
+                console.log("You Won!")
+            }
+            if (results[winner] == 'None - Draw'){
+                console.log("It was a draw...")
+            }
+            else{
+                console.log("You LOST :(")
+            }
+            },
+        error: function(xhr,status,error){
+            console.log(xhr);
+            }
+        });
+    });
+    //paper choice//
+    $('#paper').click(function(){
+        x = getRandomInt(1,3);
+        $.ajax({
+        url: `https://rps101.pythonanywhere.com/api/v1/match?object_one=paper&object_two=${rockPaperScissor(x)}`,
+        success: function(results){
+            console.log(x)
+            console.log(results);
+            },
+        error: function(xhr,status,error){
+            console.log(xhr);
+            }
+        });
+    });
+    //scissors choice//
+    $('#scissors').click(function(){
+        x = getRandomInt(1,3);
+        $.ajax({
+        url: `https://rps101.pythonanywhere.com/api/v1/match?object_one=scissors&object_two=${rockPaperScissor(x)}`,
+        success: function(results){
+            console.log(x)
+            console.log(results);
+            },
+        error: function(xhr,status,error){
+            console.log(xhr);
+            }
+        });
+    });
+});
